bac6d2daa72116aa15f91608069f06121f462350
justindavisa <justindavisa@gmail.com>
2019-02-12 18:47:18 -0500

    Calculator display built out, needs styling and props

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './App.css';
 
+import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
+
 const App = () => {
   return (
     <div>
@@ -16,6 +18,7 @@ const App = () => {
           inside of this file in order to make them work.
         </strong>
       </p>
+      <CalculatorDisplay />
     </div>
   );
 };
